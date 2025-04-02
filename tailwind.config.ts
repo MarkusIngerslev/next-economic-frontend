bcefa365951a19c74f641a96e93219206ec8833b
MarkusIngerslev <markusolsen2004@gmail.com>
2025-04-02 13:14:32 +0200

    removed not needed things for cusom tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,21 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      gridTemplateColumns: {
-        "13": "repeat(13, minmax(0, 1fr))",
-      },
       colors: {
-        blue: {
-          400: "#2589FE",
-          500: "#0070F3",
-          600: "#2F6FEB",
-        },
-      },
-    },
-    keyframes: {
-      shimmer: {
-        "100%": {
-          transform: "translateX(100%)",
+        custom: {
+          teal: "#476A6F", // Mosgrøn/teal farve
+          navy: "#151E3F", // Mørkeblå/navy farve
+          light: "#E2E2E2", // Lysegrå farve
+          purple: "#9A7AA0", // Lilla farve
+          red: "#FB3640", // Rød farve
         },
       },
     },
