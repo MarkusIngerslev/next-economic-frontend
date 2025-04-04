e9dea8419d947bbe56064c52c9796c32df08f2b0
Markus Olsen <markusolsen2004@gmail.com>
2025-04-04 14:03:30 +0200

    Added the protectedroute component as auth for dashboard and its nested routes

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,14 +1,22 @@
-import SideNav from "../ui/dashboard/sidenav";
+"use client";
+
+import SideNav from "@/app/ui/dashboard/sidenav";
+import ProtectedRoute from "@/app/ui/shared/ProtectedRoute";
 
 export const experimental_ppr = true;
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden bg-gradient-to-br from-cyan-900 to-sky-500">
-      <div className="w-full flex-none md:w-64">
-        <SideNav />
+    <ProtectedRoute>
+      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden bg-gradient-to-br from-cyan-900 to-sky-500">
+        <div className="w-full flex-none md:w-64">
+          <SideNav />
+        </div>
+
+        <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+          {children}
+        </div>
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
-    </div>
+    </ProtectedRoute>
   );
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,33 +1,22 @@
 "use client";
 
 import { useAuth } from "@/context/AuthContext";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 
 export default function DashboardPage() {
-  const { token, logout } = useAuth();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!token) {
-      router.replace("/login");
-    }
-  }, [token, router]);
-
-  if (!token) return null; // eller loading spinner
+  const { logout } = useAuth();
 
   return (
-    <main className="min-h-screen p-8 border">
-      <div className="flex items-center justify-center flex-col max-w-2xl mx-auto bg-white p-8 rounded shadow-md text-stone-600">
-        <h1 className="text-3xl font-bold mb-4 text-stone-950">Dashboard</h1>
-        <p className="mb-6">Velkommen! Du er logget ind ✅</p>
-        <button
-          onClick={logout}
-          className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700"
-        >
-          Log ud
-        </button>
-      </div>
-    </main>
+      <main className="min-h-screen p-8 border">
+        <div className="flex items-center justify-center flex-col max-w-2xl mx-auto bg-white p-8 rounded shadow-md text-stone-600">
+          <h1 className="text-3xl font-bold mb-4 text-stone-950">Dashboard</h1>
+          <p className="mb-6">Velkommen! Du er logget ind ✅</p>
+          <button
+            onClick={logout}
+            className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700"
+          >
+            Log ud
+          </button>
+        </div>
+      </main>
   );
 }
