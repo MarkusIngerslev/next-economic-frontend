b612b07785367c75b70f1513a8fc67b1943c2bd9
MarkusIngerslev <markusolsen2004@gmail.com>
2025-04-29 17:38:18 +0200

    Added rest of input fields for modal

--- src/app/ui/dashboard/profile/edit-profile-modal.tsx
+++ src/app/ui/dashboard/profile/edit-profile-modal.tsx
@@ -83,22 +83,158 @@ export default function EditProfileModal({
         exit="exit"
         onClick={(e) => e.stopPropagation()} // Forhindrer lukning ved klik inde i modalen
       >
-        <h2 className="text-xl font-semibold mb-4">Edit Profile</h2>
+        <h2 className="text-xl font-semibold mb-4">Opdater Profil</h2>
         <form>
-          <div className="mb-4">
-            <label
-              htmlFor="firstName"
-              className="block text-sm font-medium text-gray-700"
-            >
-              First Name
-            </label>
-            <input
-              type="text"
-              id="firstName"
-              defaultValue={userData.firstName}
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
+          {/* Input felter */}
+          <div className="flex flex-col mb-4">
+            {/* Personlige oplysninger */}
+            <div className="flex flex-row ">
+              <label
+                htmlFor="firstName"
+                className="block w-35 text-start content-center text-sm font-medium text-gray-700"
+              >
+                Fornavn
+              </label>
+              <input
+                type="text"
+                id="firstName"
+                defaultValue={userData.firstName}
+                className="mt-1 block w-65 content-end px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+            <div className="flex flex-row">
+              <label
+                htmlFor="lastName"
+                className="block w-35 text-start content-center text-sm font-medium text-gray-700"
+              >
+                Efternavn
+              </label>
+              <input
+                type="text"
+                id="lastName"
+                defaultValue={userData.lastName}
+                className="mt-1 block w-65 content-end px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+            <div className="flex flex-row">
+              <label
+                htmlFor="email"
+                className="block w-35 text-start content-center text-sm font-medium text-gray-700"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                defaultValue={userData.email}
+                className="mt-1 block w-65 content-end px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+            <div className="flex flex-row">
+              <label
+                htmlFor="phone"
+                className="block w-35 text-start content-center text-sm font-medium text-gray-700"
+              >
+                Telefonnummer
+              </label>
+              <input
+                type="tel"
+                id="phone"
+                defaultValue={userData.phone}
+                className="mt-1 block w-65 content-end px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+            <div className="flex flex-row">
+              <label
+                htmlFor="birthDate"
+                className="block w-35 text-start content-center text-sm font-medium text-gray-700"
+              >
+                Fødselsdato
+              </label>
+              <input
+                type="date"
+                id="birthDate"
+                defaultValue={userData.birthDate}
+                className="mt-1 block w-65 content-end px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+            {/* Adresse */}
+
+            <div className="flex flex-row">
+              <label
+                htmlFor="address"
+                className="block w-35 text-start content-center text-sm font-medium text-gray-700"
+              >
+                Adresse
+              </label>
+              <input
+                type="text"
+                id="address"
+                defaultValue={userData.address}
+                className="mt-1 block w-65 content-end px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+
+            <div className="flex flex-row">
+              <label
+                htmlFor="city"
+                className="block w-35 text-start content-center text-sm font-medium text-gray-700"
+              >
+                By
+              </label>
+              <input
+                type="text"
+                id="city"
+                defaultValue={userData.city}
+                className="mt-1 block w-65 content-end px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+            <div className="flex flex-row">
+              <label
+                htmlFor="postalCode"
+                className="block w-35 text-start content-center text-sm font-medium text-gray-700"
+              >
+                Postnummer
+              </label>
+              <input
+                type="text"
+                id="postalCode"
+                defaultValue={userData.postalCode}
+                className="mt-1 block w-65 content-end px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+            <div className="flex flex-row">
+              <label
+                htmlFor="country"
+                className="block w-35 text-start content-center text-sm font-medium text-gray-700"
+              >
+                Land
+              </label>
+              <input
+                type="text"
+                id="country"
+                defaultValue={userData.country}
+                className="mt-1 block w-65 content-end px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+
+            {/* Upload billede url */}
+            <div className="flex flex-row">
+              <label
+                htmlFor="profilePictureUrl"
+                className="block w-35 text-start content-center text-sm font-medium text-gray-700"
+              >
+                Profilbillede
+              </label>
+              <input
+                type="text"
+                id="profilePictureUrl"
+                defaultValue={userData.profilePictureUrl}
+                className="mt-1 block w-65 content-end px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
           </div>
+
           {/* ... andre form felter ... */}
           <div className="flex justify-end gap-4 mt-6">
             <button
@@ -106,14 +242,14 @@ export default function EditProfileModal({
               onClick={onClose}
               className="px-4 py-2 bg-gray-300 text-gray-800 rounded hover:bg-gray-400"
             >
-              Cancel
+              Annuller
             </button>
             <button
               type="button"
               onClick={handleSave}
               className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
             >
-              Save Changes
+              Gem ændringer
             </button>
           </div>
         </form>
