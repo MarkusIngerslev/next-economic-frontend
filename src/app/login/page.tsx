a8bfe802595cbac79554ff76fcd330e76d3ae6ab
MarkusIngerslev <markusolsen2004@gmail.com>
2025-05-19 16:22:38 +0200

    Changed background color of login page to use same default color gray-800 as used on dashboard

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,13 +5,15 @@ import dynamic from "next/dynamic";
 // Importing the LoginForm component dynamically to avoid server-side rendering
 const LoginForm = dynamic(
   () =>
-    import("@/app/ui/shared/login-form").then((mod) => ({ default: mod.LoginForm })),
+    import("@/app/ui/shared/login-form").then((mod) => ({
+      default: mod.LoginForm,
+    })),
   { ssr: false }
 );
 
 export default function LoginPage() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-600 to-violet-400 px-4">
+    <main className="min-h-screen flex items-center justify-center bg-gray-800 px-4">
       <LoginForm />
     </main>
   );
