4fe8bf53d75cd526a43e7e5710e36cc8a2cf8500
MarkusIngerslev <markusolsen2004@gmail.com>
2025-05-20 14:44:37 +0200

    Created placeholder register page

---
+++ src/app/register/page.tsx
@@ -0,0 +1,11 @@
+export default function page() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p className="text-lg mb-4">
+        This is a test page for the register functionality.
+      </p>
+      <p className="text-lg">More content will be added here soon.</p>
+    </main>
+  );
+}
